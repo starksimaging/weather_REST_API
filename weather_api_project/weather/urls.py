90e2c67117965c2e460bac8ed565d2aa35db590d
jon <jonstarks6@gmail.com>
2025-03-27 04:38:00 -0400

    Add weather form view and template; update API call to use imperial units

--- weather_api_project/weather/urls.py
+++ weather_api_project/weather/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import WeatherView
+from .views import weather_form
 
 urlpatterns = [
-    path('get-weather/', WeatherView.as_view(), name='get-weather')
+    path('get-weather/', WeatherView.as_view(), name='get-weather'),
+    path('', weather_form, name='weather-form')
 ]
--- weather_api_project/weather/views.py
+++ weather_api_project/weather/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import CitySerializer
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
@@ -14,7 +15,7 @@ class WeatherView(APIView):
         serializer = CitySerializer(data=request.data)
         if serializer.is_valid():
             city = serializer.validated_data['city']
-            url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_API_KEY}&units=metric'
+            url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_API_KEY}&units=imperial'
             response = requests.get(url)
             if response.status_code == 200:
                 data = response.json()
@@ -27,4 +28,27 @@ class WeatherView(APIView):
             else:
                 return Response({'error': 'City not found'}, status= status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@csrf_exempt
+def weather_form(request):
+        weather = None
+        error = None
+
+        if request.method == 'POST':
+            city = request.POST.get('city')
+            if city:
+                url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_API_KEY}&units=imperial'
+                response = requests.get(url)
+                if response.status_code == 200:
+                    data = response.json()
+                    weather = {
+                        'city': city,
+                        'temperature': data['main']['temp'],
+                        'description': data['weather'][0]['description'],
+                    }
+                else:
+                    error = 'City not found'
+        return render(request, 'weather/weather_form.html', {'weather': weather, 'error': error})
+
 
